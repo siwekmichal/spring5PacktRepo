3fc811f9082536a338948b3e2e37159798b0dec8
msiwek <msiwek@cyfrowypolsat.pl>
2020-03-06 16:29:58 +0100

    ch04 - AuthFilter - dodanie CustomFailureHandler (poprawki)

--- ch04/src/main/java/org/packt/secured/mvc/core/handler/CustomFailureHandler.java
+++ ch04/src/main/java/org/packt/secured/mvc/core/handler/CustomFailureHandler.java
@@ -19,7 +19,7 @@ public class CustomFailureHandler extends SimpleUrlAuthenticationFailureHandler 
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        super.onAuthenticationFailure(request, response, exception)
+
         System.out.println("failure");
 
         String targetUrl = "";
